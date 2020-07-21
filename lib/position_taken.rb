78f899a6168532250844dc9bff0bc98bff43fb9c
Sierra Wallace <sskwallace@gmail.com>
2020-07-21 21:14:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,8 @@
 def position_taken?(board, index)
-  if board[index] == " " ||  board[index] == "" 
+  if board[index] == " " ||  board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O"
     return true
-  else
-    return nil
   end
 end
 
